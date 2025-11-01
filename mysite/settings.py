e1930affdb33944a9d86ce71a895cc04d45dfd9a
Jonathan Kevin <jonathakevin27@gmail.com>
2025-11-01 11:53:46 -0600

    Enhance MySQL database settings by adding connection timeout and initializing SQL mode; remove isolation level and test charset options.

--- mysite/settings.py
+++ mysite/settings.py
@@ -101,17 +101,14 @@ DATABASES = {
         "PASSWORD": os.getenv("MYSQLPASSWORD"),
         "HOST": os.getenv("MYSQLHOST"),
         "PORT": os.getenv("MYSQLPORT"),
-        "OPTIONS": {
+        'CONN_MAX_AGE': None,
+        'OPTIONS': {
+            "connect_timeout": 20,
             "charset": "utf8mb4",
-            "isolation_level": "read committed",
-        },
-        'TEST': {
-            'CHARSET': 'utf8mb4',
-            'COLLATION': 'utf8mb4_general_ci',
+            "init_command": "SET sql_mode='STRICT_TRANS_TABLES'",
         }
     }
 }
-
 
 
 
